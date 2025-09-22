1499a1c08a596ab1f23700f1521fa7a2a32902ea
wangran326990 <wangrancanada107@gmail.com>
2025-09-22 18:46:14 -0400

    report azure function

---
+++ email-report-timer/function_app.py
@@ -0,0 +1,91 @@
+from datetime import timedelta, timezone, datetime
+from email.message import EmailMessage
+import logging
+import mimetypes
+from pathlib import Path
+import smtplib
+from typing import List
+import azure.functions as func
+from utils.storage_table_entities import EmailAttachmentEntity
+from utils.storage_table_util import StorageTableUtil
+import pandas as pd
+from dataclasses import asdict
+import os
+app = func.FunctionApp()
+
+@app.timer_trigger(schedule="0 */1 * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def daily_report_timer(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger daily report function executed.')
+    generate_report()
+
+def generate_report() -> str:
+    table_name = os.getenv("STORAGE_TABLE_NAME")
+    # Fetch all attachments processed in the last 24 hours
+    attachments:List[EmailAttachmentEntity] = StorageTableUtil.get_all_attachments_processed_in_24hrs(table_name)
+    if not attachments:
+        logging.info("No attachments processed in the last 24 hours.")
+        return "No attachments processed in the last 24 hours."
+    # Convert to Excel
+    excel_file = convert_to_excel(attachments)
+    # send email with excel attachment
+    send_gmail_with_attachment(excel_file)
+    
+    # Mark all as reported
+    mark_attachments_as_reported(attachments, table_name)
+    return "Report generated and email sent."  
+
+
+def convert_to_excel(attachments: List[EmailAttachmentEntity]) -> str:
+    data_dicts = [
+        {k: v for k, v in asdict(entity).items() if k != "isReported" and k != "processDateTime"}
+        for entity in attachments
+    ]
+
+    # Create a DataFrame
+    df = pd.DataFrame(data_dicts)
+    excel_file_name = os.getenv("EXCEL_FILE_NAME", "daily_report.xlsx")
+    # Save to Excel
+    df.to_excel(excel_file_name, index=False)
+    return excel_file_name
+
+def send_gmail_with_attachment(excel_file: str):
+    SMTP_SERVER = "smtp.gmail.com"       # e.g., Gmail SMTP
+    SMTP_PORT = 587
+    SENDER_EMAIL = os.getenv("SENDER_EMAIL")  # Your Gmail address
+    SENDER_PASSWORD = os.getenv("APP_PASSWORD")  # Your Gmail App Password
+    TO_EMAIL = os.getenv("TO_EMAIL")  # Recipient's email address
+    msg = EmailMessage()
+    msg['Subject'] = "Test Email with Attachment"
+    msg['From'] = SENDER_EMAIL
+    msg['To'] = TO_EMAIL
+    msg.set_content("Hello,\n\nThis is the daily report for processing vendors emails.\n\nRegards")
+    file_path = Path(excel_file)
+    mime_type, _ = mimetypes.guess_type(file_path)
+    mime_type = mime_type or "application/octet-stream"
+    maintype, subtype = mime_type.split('/', 1)
+
+    with open(excel_file, 'rb') as f:
+        msg.add_attachment(f.read(),
+                        maintype=maintype,
+                        subtype=subtype,
+                        filename=file_path.name)
+        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.starttls()  # upgrade to secure connection
+            server.login(SENDER_EMAIL, SENDER_PASSWORD)
+            server.send_message(msg)
+
+            logging.info("Email sent successfully!")
+
+def mark_attachments_as_reported(attachments: List[EmailAttachmentEntity], table_name: str):
+    entities = []
+    for attachment in attachments:
+
+        attachment.isReported = True
+        attachment.processDateTime = datetime.now(timezone.utc)
+        entities.append(attachment.__dict__)
+    
+    StorageTableUtil.batch_update_entity(table_name, entities)
---
+++ email-report-timer/utils/storage_table_util.py
@@ -0,0 +1,91 @@
+from typing import List
+from azure.data.tables import TableServiceClient, TableClient, UpdateMode, TableTransactionError
+from datetime import timedelta, timezone, datetime
+import os
+import logging
+
+from utils.email_dtos import FileAttachment
+from utils.storage_table_entities import EmailAttachmentEntity
+
+class StorageTableUtil:
+    table_service = None
+    
+    @classmethod
+    def get_connection_string(cls)-> str:
+        return  os.environ["AzureWebJobsStorage"]
+
+    @classmethod
+    def get_table_service(cls):
+        if cls.table_service is None:
+            
+            cls.table_service = TableServiceClient.from_connection_string(cls.get_connection_string())
+        return cls.table_service
+    
+    @classmethod
+    def ensure_table_exists(cls, table_name: str):
+        table_service = cls.get_table_service()
+        try:
+            table_service.create_table_if_not_exists(table_name=table_name)
+        except Exception as e:
+            raise Exception(f"Error creating or accessing table {table_name}: {str(e)}")
+        
+    @classmethod
+    def get_table_client(cls, table_name: str)-> TableClient:
+        cls.ensure_table_exists(table_name)
+        table_service = cls.get_table_service()
+        return table_service.get_table_client(table_name)
+    
+    @classmethod
+    def insert_entity(cls, table_name: str, entity: dict):
+        table_client = cls.get_table_client(table_name)
+        try:
+            table_client.create_entity(entity=entity)
+        except Exception as e:
+            raise Exception(f"Error inserting entity into table {table_name}: {str(e)}")
+        
+    @classmethod
+    def get_all_attachments_processed_in_24hrs(cls, table_name: str)-> List[EmailAttachmentEntity]:
+        
+        table_client = cls.get_table_client(table_name)
+        try:
+            twenty_four_hours_ago = datetime.now(timezone.utc) - timedelta(hours=24)
+            filter_query = (
+                f"processDateTime ge '{twenty_four_hours_ago.isoformat()}' "
+                f"and isReported eq false"
+            )
+            entities = table_client.query_entities(query_filter=filter_query)
+            return [EmailAttachmentEntity(**entity) for entity in entities]
+        except Exception as e:
+            raise Exception(f"Error querying entities from table {table_name}: {str(e)}")
+    
+    @classmethod
+    def batch_update_entity(cls, table_name: str, entities: list):
+        table_client:TableClient = cls.get_table_client(table_name)
+        batch_ops = [
+        ("update", entity, {"mode": UpdateMode.MERGE}) for entity in entities
+        ]
+        try:
+            responses = table_client.submit_transaction(batch_ops)
+            return responses
+        except TableTransactionError as e:
+            # - e.message : error message
+            # - e.response : HTTP response from the server
+            logging.error("Batch update failed!")
+            logging.error("Error message:", e.message)
+            
+            # The failed operation details are inside e.response
+            logging.error("Failed operation HTTP status:", e.response.status_code)
+            logging.error("Failed operation body:", e.response.text)
+
+    @classmethod
+    def get_entity(cls, table_name: str, partition_key: str, row_key: str) -> dict:
+        table_client = cls.get_table_client(table_name)
+        try:
+            entity = table_client.get_entity(partition_key=partition_key, row_key=row_key)
+            return entity
+        except Exception as e:
+            logging.error(f"Error retrieving entity from table {table_name}: {str(e)}")
+            return None
+
+    
+    
